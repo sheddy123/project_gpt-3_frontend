2657e83fb63f865eba9b00701a440d15be0bbfb4
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-06-26 19:24:19 +0100

    Added components/PossibilityComponent

---
+++ src/components/Possibility/PossibilityComponent/Possibility.tsx
@@ -0,0 +1,17 @@
+import possibilityImage from '../../../assets/images/manufacturing-hero.png';
+import './Possibility.css';
+import CodeEditor from '../../Common/CodeEditor/CodeEditor';
+
+const Possibility = () => (
+  <div className="gpt3__possibility section__padding" id="possibility">
+    <div className="gpt3__possibility-image">
+      <img src={possibilityImage} alt="possibility" />
+    </div>
+    <div className="gpt3__possibility-content">
+      
+      <CodeEditor />
+    </div>
+  </div>
+);
+
+export default Possibility;
