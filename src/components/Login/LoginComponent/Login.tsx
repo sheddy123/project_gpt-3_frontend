8309954cc049c3e2c5f015489a5ab1c3eae760ed
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-06-29 21:50:44 +0100

    COmmit all

--- src/components/Login/LoginComponent/Login.tsx
+++ src/components/Login/LoginComponent/Login.tsx
@@ -4,6 +4,10 @@ import { useSelector, useDispatch } from "react-redux";
 import { getAuth } from "../../../redux/features/auth/authSlice";
 import Modal from "../../Common/Modal/Modal";
 import { openModal } from "../../../redux/features/modal/modalSlice";
+import { unwrapResult } from "@reduxjs/toolkit";
+import useAuth from "../../../utils/Hooks/useAuth";
+
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const url = import.meta.env.VITE_CLIENT_ID_URL + import.meta.env.VITE_CLIENT_ID;
 //"https://localhost:7135/api/Auth/Authenticate?code="
@@ -11,24 +15,40 @@ function loginWithGitHub(dispatch) {
   //  dispatch(openModal(null));
   window.location.assign(url);
 }
+//const navigate = useNavigate();
 
 const Login = () => {
   const dispatch = useDispatch();
-  //const navigate = useNavigate();
-  const store = useSelector((state) => state);
+  const modalReducer = useSelector((state) => state.modalReducer);
+  const { setAuth, auth } = useAuth();
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const codeParam = urlParams.get("code");
     if (codeParam) {
-      dispatch(getAuth(codeParam) as any).then(() => {
-        // Clear the URL parameter after successful API call
-        // navigate('/login', { replace: true });
-      });
+      dispatch(getAuth(codeParam) as any)
+        .then(unwrapResult)
+        .then((result) => {
+          // Clear the URL parameter after successful API call
+          //navigate('/', { replace: true });
+          setAuth({
+            message: result?.message,
+            auth_response: result?.auth_User,
+          });
+          navigate(from, { replace: true });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
   }, [dispatch]);
-  const modal = store.modalReducer.isOpen && <Modal />;
+  const modal = modalReducer.isOpen && <Modal />;
+
   return (
     <>
       {modal}
--- src/interfaces/IFeatures/IFeatures.tsx
+++ src/interfaces/IFeatures/IFeatures.tsx
@@ -1,5 +1,27 @@
 export interface IAuth {
-    isLoading: boolean;
-    code: string;
-    message: string;
+  message: string;
+  auth_response: IAuthResponse;
+}
+export interface IAuthResponse {
+  isLoading: boolean;
+  code: string;
+  access_failed_count: string;
+  email: string;
+  locked_out_enabled: string;
+  lockout_end: string;
+  normalized_email: string;
+  normalized_username: string;
+  phone_number: string;
+  security_stamp: string;
+  user_name: string;
+  twoFactorEnabled: string;
+  roles: IRoles[];
+  token: string;
+  refresh_token: string;
+  refresh_token_expiry_time: string;
+}
+
+export interface IRoles {
+  id: number;
+  roleName: string;
 }
