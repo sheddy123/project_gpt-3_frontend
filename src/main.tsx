9f39e46e61afccfd78a1f13d247b1dfc653f7653
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-05 21:26:38 +0100

    Added a ContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,18 @@ import "./styles/main.css";
 import { Provider } from "react-redux";
 import store from "./redux/store/store.tsx";
 import { AuthProvider } from "./utils/Helpers/authprovider.tsx";
+import { ContextProvider } from "./utils/Helpers/ContextProvider.tsx";
 import RoutePages from "./routes/RoutesPages.tsx";
 import { BrowserRouter } from "react-router-dom";
-
-
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   //<React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
       <AuthProvider>
-        <RoutePages />
+        <ContextProvider>
+          <RoutePages />
+        </ContextProvider>
       </AuthProvider>
     </BrowserRouter>
   </Provider>
