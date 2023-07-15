11c6f52ef968e85b73c39a6a34b436e7c834aea1
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-15 13:46:30 +0100

    Update Modal.tsx

--- src/components/Common/Modal/Modal.tsx
+++ src/components/Common/Modal/Modal.tsx
@@ -5,51 +5,57 @@ import { Close } from "../Icons/Icons";
 import OAuthGithub from "../OAuthProviders/OAuthGithub";
 import OAuthGoogle from "../OAuthProviders/OAuthGoogle";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Modal = () => {
   const dispatch = useDispatch();
   const [errorText, setErrorText] = useState("");
   const errorResponse = useSelector((store) => store?.authReducer?.message);
   const [showAlert, setShowAlert] = useState(false);
-
+  const location = useLocation();
+  const message = location.state && location.state.message;
+  
   useEffect(() => {
     if (
       typeof errorResponse === "string" &&
-      errorResponse.includes("Lifetime validation failed")
+      (errorResponse.includes("Lifetime validation failed") || message === "Not Authenticated" || message === "Session Ended")
     ) {
       // The string "Lifetime validation failed" is present in the variable
       setErrorText("Session Ended");
       setShowAlert(true);
       const timer = setTimeout(() => {
         setShowAlert(false);
-      }, 15000);
+      }, 5000);
       return () => {
         clearTimeout(timer);
       };
     }
-    
   }, []);
   //console.log("The variable contains 'Lifetime validation failed'" + errorText);
-  
+
   return (
     <aside className="modal-container ">
       <div className="modal ">
-      <div
-            className={`bg-red-100 border alert-container animate__animated  ${showAlert ? 'animate__fadeInDown' : 'animate__fadeOutUp'} border-red-400 text-red-700 px-4 py-3 rounded relative`}
+        {showAlert && (
+          <div
+            className={`bg-red-100 border alert-container animate__animated  ${
+              showAlert ? "animate__fadeInDown" : "animate__fadeOutUp"
+            } border-red-400 text-red-700 px-4 py-3 rounded relative`}
             role="alert">
-            <strong className="font-bold">Session Ended!</strong>
+            <strong className="font-bold">{message}</strong>
           </div>
+        )}
         <div className="header">
           <h4>Signup with one of the following:</h4>
 
           <button
-            className="close-icon"
+            className="close-icon hover:drop-shadow-xl hover:bg-light-gray"
+            style={{ borderRadius: "50%" }}
             onClick={() => dispatch(closeModal(null))}>
             <Close />
           </button>
         </div>
         <div className="content">
-        
           {/* <div className="mt-4">
             <OAuthMicrosoft />
           </div> */}
