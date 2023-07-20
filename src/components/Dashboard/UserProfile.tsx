a2c18c8b7db2433b400fb01bad2db199544665cb
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-20 10:00:21 +0100

    Update UserProfile.tsx

--- src/components/Dashboard/UserProfile.tsx
+++ src/components/Dashboard/UserProfile.tsx
@@ -1,19 +1,21 @@
-import { MdOutlineCancel } from 'react-icons/md';
+import { MdOutlineCancel } from "react-icons/md";
 
-import { Button } from '..';
-import { userProfileData } from './data/dummy';
-import { useStateContext } from '@/utils/Helpers/ContextProvider';
-import { useSelector } from 'react-redux';
-import { IAuth } from '@/interfaces/IFeatures/IFeatures';
-import useLogout from '@/utils/Hooks/useLogout';
+import { Button } from "..";
+import { userProfileData } from "./data/dummy";
+import { useStateContext } from "@/utils/Helpers/ContextProvider";
+import { useSelector } from "react-redux";
+import useLogout from "@/utils/Hooks/useLogout";
+import { MapPin, Verified } from "../Common/Icons/Icons";
 
 const UserProfile = () => {
-  const { currentColor } = useStateContext();
+  const { currentColor, currentMode } = useStateContext();
+  
   const logout = useLogout();
   const logUserOut = async () => {
     await logout();
   };
-  const store: IAuth | undefined = useSelector((store) => store?.authReducer);
+  const store = useSelector((store) => store);
+
   return (
     <div className="nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
       <div className="flex justify-between items-center">
@@ -29,46 +31,104 @@ const UserProfile = () => {
       <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
         <img
           className="rounded-full h-24 w-24"
-          src={store?.auth_response.image_url}
+          src={store?.authReducer?.auth_response.image_url}
           alt="user-profile"
         />
         <div>
-          <p className="font-semibold text-xl dark:text-gray-200"> {store?.auth_response.user_name} </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400">  {store?.auth_response.roles?.[0].roleName}   </p>
-          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> {store?.auth_response.email} </p>
+          <p className="font-semibold text-xl dark:text-gray-200">
+            {" "}
+            {store?.authReducer?.auth_response.user_name}
+            <span className="inline-flex items-center justify-center w-5 h-5 ml-1">
+              <Verified color="#31d26c" size={5} />
+            </span>{" "}
+          </p>
+          <p className="text-gray-500 text-sm dark:text-gray-400">
+            {" "}
+            {store?.authReducer?.auth_response.roles?.[0].roleName}{" "}
+            <MapPin color={currentMode === "Dark" ? '#fff' : currentColor} size="5" />
+          </p>
         </div>
+
+        <p className="text-gray-500 text-sm font-semibold dark:text-gray-400">
+          {" "}
+          {store?.authReducer?.auth_response.email}{" "}
+        </p>
       </div>
+
       <div>
+      
+<ol className="relative border-l border-gray-200 dark:border-gray-700">                  
+    <li className="mb-10 ml-6">            
+        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Application UI v2.0.0 <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Latest</span></h3>
+        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 13th, 2022</time>
+        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+        <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"><svg className="w-3.5 h-3.5 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z"/>
+    <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
+  </svg> Download ZIP</a>
+    </li>
+    <li className="mb-10 ml-6">
+        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Figma v1.3.0</h3>
+        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 7th, 2021</time>
+        <p className="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+    </li>
+    <li className="ml-6">
+        <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.2</h3>
+        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 2nd, 2021</time>
+        <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+    </li>
+</ol>
+
         {userProfileData.map((item, index) => (
-          <div key={index} className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
+          <div
+            key={index}
+            className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
             <button
               type="button"
               style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-              className=" text-xl rounded-lg p-3 hover:bg-light-gray"
-            >
+              className=" text-xl rounded-lg p-3 hover:bg-light-gray">
               {item.icon}
             </button>
 
             <div>
               <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.desc} </p>
+              <p className="text-gray-500 text-sm dark:text-gray-400">
+                {" "}
+                {item.desc}{" "}
+              </p>
             </div>
           </div>
         ))}
       </div>
       <div className="mt-5">
-      <button
-      type="button"
-      onClick={logout}
-      style={{ backgroundColor: currentColor, color: 'white', borderRadius:'10px' }}
-      className={` text- p-3 w-full hover:drop-shadow-xl hover:bg-`}
-    >
-      Logout
-    </button>
-        
+        <button
+          type="button"
+          onClick={logout}
+          style={{
+            backgroundColor: currentColor,
+            color: "white",
+            borderRadius: "10px",
+          }}
+          className={` text- p-3 w-full hover:drop-shadow-xl hover:bg-`}>
+          Logout
+        </button>
       </div>
     </div>
-
   );
 };
 
