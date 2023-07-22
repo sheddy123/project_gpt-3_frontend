3410e5f28bf6cdb32060ebc30313bb5cc718985d
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-22 08:24:19 +0100

    Create index.tsx

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,4 @@
+export { default as CoursesList } from "./CoursesList";
+export { default as MyLearningProgress } from "./MyLearning";
+export { default as Leaders } from "./Leaders";
+export { default as YouMayLike } from "./YouMayLike";
