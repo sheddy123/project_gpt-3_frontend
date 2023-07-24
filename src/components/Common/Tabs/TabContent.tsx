83cdf617beb64f72bca936c4b931662d5adda62b
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-24 09:06:01 +0100

    Update TabContent.tsx

--- src/components/Common/Tabs/TabContent.tsx
+++ src/components/Common/Tabs/TabContent.tsx
@@ -1,8 +1,10 @@
 import { earningData } from "@/components/Dashboard/data/dummy";
 import { useStateContext } from "@/utils/Helpers/ContextProvider";
 import { ProgressBarComponent } from "@syncfusion/ej2-react-progressbar";
-
+import "./Tabs.css";
 import { IoIosMore } from "react-icons/io";
+import Button from "../Button/Button";
+import { NavLink } from "react-router-dom";
 const TabContent = ({ TabNavs, activeTab }) => {
   const { currentColor } = useStateContext();
   return (
@@ -13,7 +15,7 @@ const TabContent = ({ TabNavs, activeTab }) => {
             <div className="grid grid-cols-3 gap-4 mt-3 rounded-sm items-center">
               {earningData.map((item, index) => (
                 <>
-                  <div className="bg-gray-50 shadow grid grid-cols-2 p-2">
+                  <div className="bg-gray-50 shadow grid grid-cols-2 p-2 h-full w-full overflow-hidden relative max-w-xs bg-cover bg-no-repeat cont">
                     <div>
                       <button
                         type="button"
@@ -53,6 +55,13 @@ const TabContent = ({ TabNavs, activeTab }) => {
                         style={{ float: "inline-end" }}>
                         1/2
                       </span>
+                    </div>
+                    <div className="group fd-sh group-hover:opacity-100 absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden  bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-20 flex justify-center items-center">
+                      <NavLink to={`/dashboard/courses/${item.title.toLowerCase()}`} className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded shadow'}>
+                        Get started
+                      </NavLink>
+                      {/* <Button styles="opacity-0.9 cursor-pointer" text="Get started" /> */}
+                      <div className="absolute inset-0 bg-slate-500 opacity-0 group-hover:opacity-20 transition duration-300 ease-in-out pointer-events-none"></div>
                     </div>
                   </div>
                 </>
