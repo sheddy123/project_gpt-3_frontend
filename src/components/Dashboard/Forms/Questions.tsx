d6de4a88ec46beaf90a6f5fdc413dc66b1a973cd
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-28 21:06:46 +0100

    Update Questions.tsx

--- src/components/Dashboard/Forms/Questions.tsx
+++ src/components/Dashboard/Forms/Questions.tsx
@@ -4,6 +4,8 @@ import { Star } from "@/components/Common/Icons/Icons";
 import { useState } from "react";
 import DOMPurify from "dompurify";
 import QuestionListAnswerPanel from "./QuestionListAnswerPanel";
+import { setPage, setSkippedQuestion } from "@/redux/features/form/formSlice";
+import { useDispatch } from "react-redux";
 
 const Questions = ({
   component,
@@ -15,52 +17,51 @@ const Questions = ({
   handleNext,
   handlePrev,
   page,
-  selectedQuestion
+  selectedQuestion,
+  selectAnswerOption,
+  selectAnsweredQuestions,
+  selectQuestionsSkipped,
 }) => {
   const { currentColor, currentMode } = useStateContext();
   const [fillColor, setFillColor] = useState("slate-400");
   const [selectedItem, setSelectedItem] = useState(null);
-  const sanitizedText = DOMPurify.sanitize(data);
-  
+  const sanitizedText = DOMPurify.sanitize(data.question);
+  const dispatch = useDispatch();
 
+  const currentQuestionAnsweredObject = selectAnsweredQuestions.find(
+    (item) => item.id === component
+  );
+  const currentQuestionAnswered =
+    currentQuestionAnsweredObject?.id == component
+      ? currentQuestionAnsweredObject.selectedAnswer
+      : "";
+  const currentQuestionAnsweredClass =
+    currentQuestionAnsweredObject?.id == component
+      ? "bg-black text-[#FAEBD7] dark:bg-[#20232a] dark:border-[#20232a] dark:border-[white]"
+      : "";
+  const requiredQuestionIds = Array.from({ length: 5 }, (_, index) => index);
+  const matchingObjects = selectAnsweredQuestions.filter((item) =>
+    requiredQuestionIds.includes(item.id)
+  );
+
+  const questionTextAnswered =
+    matchingObjects.length <= 1 ? "question" : "questions";
+
+  //const numberOfQuestionsSkipped = matchingObjects.length
   const handleItemClick = (item) => {
-    setSelectedItem(item);
+    const { selectedAnswer } = item;
+
+    setSelectedItem(selectedAnswer);
+
+    dispatch(selectAnswerOption(item));
   };
 
-  const handleClick = () => {
-    
-    // Toggle the fillColor when the li is clicked
-    // setFillColor((prevColor) =>
-    //   prevColor === "slate-400" ? "teal-500" : "slate-400"
-    // );
+  const handleClick = (page: number) => {
+    if (page > 1) {
+      dispatch(setSkippedQuestion(page));
+    }
+    dispatch(setPage(page + 1));
   };
-
-  //   const renderBillingContent = () => {
-  //     switch (component) {
-  //       case "Billing1":
-  //         return (
-  //           <div>
-  //             {/* Billing1 Content */}
-  //             <h2>Billing 1</h2>
-  //             {/* Use the data prop as needed */}
-  //             <p>{data}</p>
-  //           </div>
-  //         );
-  //       case "Billing2":
-  //         return (
-  //           <div>
-  //             {/* Billing2 Content */}
-  //             <h2>Billing 2</h2>
-  //             {/* Use the data prop as needed */}
-  //             <p>{data}</p>
-  //           </div>
-  //         );
-  //       // Add more cases for additional billing components as needed
-  //       default:
-  //         return null;
-  //     }
-  //   };
-
   return (
     <div>
       <div>
@@ -77,9 +78,7 @@ const Questions = ({
       {/* Example: Billing navigation buttons */}
       <div className={`grid grid-cols-1 sm:grid-cols-3 gap-4`}>
         <div className="col-span-2  dark:text-gray-200  py-4 px-6 rounded-md p-3 mt-3">
-          <span
-            className={`flex flex-row items-center cursor-pointer`}
-            onClick={handleClick}>
+          <span className={`flex flex-row items-center`}>
             <Star
               currentMode={currentMode}
               currentColor={currentColor}
@@ -96,20 +95,34 @@ const Questions = ({
             {options.map((item, index) => (
               <li
                 key={index}
-                className={`border border-[#676161]  p-4 rounded-lg flex items-center cursor-pointer w-full ${
-                  selectedItem === item &&
-                  "dark:bg-[#20232a] dark:border-[#20232a] bg-black text-[#FAEBD7]"
-                } dark:border-[white]`}
+                className={`border border-[#676161]  p-4 rounded-lg flex items-center cursor-pointer w-full hover:bg-black hover:text-[#FAEBD7] hover:dark:bg-[#20232a] hover:dark:border-[#20232a]
+                ${
+                  selectedItem === item || currentQuestionAnswered == item
+                    ? "dark:bg-[#20232a] dark:border-[#20232a] bg-black text-[#FAEBD7] " +
+                      currentQuestionAnsweredClass
+                    : ""
+                } 
+                
+                dark:border-[white] hover:dark:border-[white]`}
                 style={{
                   borderColor:
                     selectedItem === item && currentMode == "Dark"
                       ? "#20232a"
                       : "",
                 }}
-                onClick={() => handleItemClick(item)}>
-                <div className="w-4 h-4 border border-black dark:border-[#FFF8DC] rounded-full mr-2">
-                  {selectedItem === item && (
+                onClick={() =>
+                  handleItemClick({
+                    selectedAnswer: item,
+                    id: data.id,
+                    questionId: data.formId,
+                    courseId: data.courseId,
+                  })
+                }>
+                <div className="w-4 h-4 border hover_answer_div border-black dark:border-[#FFF8DC] rounded-full mr-2 hover:w-4 hover:h-4 hover:border hover:border-white hover:dark:border-[#FFF8DC] hover:rounded-full hover:mr-2">
+                  {selectedItem === item || currentQuestionAnswered == item ? (
                     <div className="w-full h-full bg-[#FAEBD7] dark:bg-[#FFF8DC] rounded-full"></div>
+                  ) : (
+                    <div className="hover:w-full hover:h-full hover:bg-[#FAEBD7] hover:dark:bg-[#FFF8DC] hover:rounded-full"></div>
                   )}
                 </div>
                 {item}
@@ -117,16 +130,26 @@ const Questions = ({
             ))}
           </ul>
           <div className="flex gap-4 float-right">
-            <button
-              onClick={() => handlePrev(page)}
-              className="flex-1 px-4 py-2 rounded-sm bg-transparent hover:bg-gray-100 text-gray-600 hover:text-gray-800 border border-gray-300 hover:border-gray-400 transition duration-300">
-              Previous
-            </button>
-            <button
-              onClick={() => handleNext(page)}
-              className="flex-1 px-4 py-2 rounded-sm bg-transparent hover:bg-gray-100 text-gray-600 hover:text-gray-800 border border-gray-300 hover:border-gray-400 transition duration-300">
-              Next
-            </button>
+            {page != 2 && (
+              <button
+                onClick={() => handlePrev(page)}
+                className="flex-1 px-4 py-2 rounded-sm bg-transparent hover:bg-gray-100 text-gray-600 hover:text-gray-800 border border-gray-300 hover:border-gray-400 transition duration-300">
+                Previous
+              </button>
+            )}
+            {component == 5 ? (
+              <button
+                onClick={() => handleNext(page)}
+                className="flex-1 px-4 py-2 rounded-sm bg-transparent hover:bg-gray-100 text-gray-600 hover:text-gray-800 border border-gray-300 hover:border-gray-400 transition duration-300">
+                Submit
+              </button>
+            ) : (
+              <button
+                onClick={() => handleNext(page)}
+                className="flex-1 px-4 py-2 rounded-sm bg-transparent hover:bg-gray-100 text-gray-600 hover:text-gray-800 border border-gray-300 hover:border-gray-400 transition duration-300">
+                Next
+              </button>
+            )}
           </div>
         </div>
 
@@ -153,12 +176,14 @@ const Questions = ({
                 handleClick={handleClick}
                 component={component}
                 selectedQuestion={selectedQuestion}
+                selectQuestionsSkipped={selectQuestionsSkipped}
               />
             ))}
           </ul>
           <div className="flex justify-between items-center mt-5 border-t-1 border-color">
             <p className=" text-gray-500 text-sm px-6 py-3">
-              2 questions skipped
+              {selectAnsweredQuestions.length - 1} {questionTextAnswered}{" "}
+              answered
             </p>
           </div>
         </div>
