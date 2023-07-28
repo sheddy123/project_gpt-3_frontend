3e4d0687347a3d58836165d2c74aad17c301d290
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-28 10:23:58 +0100

    Update Questions.tsx

--- src/components/Dashboard/Forms/Questions.tsx
+++ src/components/Dashboard/Forms/Questions.tsx
@@ -1,28 +1,38 @@
-import { selectFormTotalQuestionsLength } from "@/redux/features/form/formSlice";
-import { useSelector } from "react-redux";
 import { useStateContext } from "@/utils/Helpers/ContextProvider";
 import { AiOutlineCaretDown } from "react-icons/ai";
 import { Star } from "@/components/Common/Icons/Icons";
 import { useState } from "react";
-import DOMPurify from 'dompurify';
+import DOMPurify from "dompurify";
+import QuestionListAnswerPanel from "./QuestionListAnswerPanel";
 
-const Questions = ({ component, data, handleChange, options, answer }) => {
-  const questLength = useSelector(selectFormTotalQuestionsLength);
+const Questions = ({
+  component,
+  data,
+  handleChange,
+  options,
+  answer,
+  questLength,
+  handleNext,
+  handlePrev,
+  page,
+  selectedQuestion
+}) => {
   const { currentColor, currentMode } = useStateContext();
   const [fillColor, setFillColor] = useState("slate-400");
   const [selectedItem, setSelectedItem] = useState(null);
-  const items = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6'];
-
   const sanitizedText = DOMPurify.sanitize(data);
+  
+
   const handleItemClick = (item) => {
     setSelectedItem(item);
   };
 
   const handleClick = () => {
+    
     // Toggle the fillColor when the li is clicked
-    setFillColor((prevColor) =>
-      prevColor === "slate-400" ? "teal-500" : "slate-400"
-    );
+    // setFillColor((prevColor) =>
+    //   prevColor === "slate-400" ? "teal-500" : "slate-400"
+    // );
   };
 
   //   const renderBillingContent = () => {
@@ -57,12 +67,12 @@ const Questions = ({ component, data, handleChange, options, answer }) => {
         {/* Billing1 Content */}
         <h2>Short Quiz</h2>
         <h4>
-          Quiz {component} | {questLength} questions
+          Quiz {component} | {questLength - 2} questions
         </h4>
 
         {/* Use the data prop as needed */}
       </div>
-      
+
       {/* Common Billing UI elements can be placed here if needed */}
       {/* Example: Billing navigation buttons */}
       <div className={`grid grid-cols-1 sm:grid-cols-3 gap-4`}>
@@ -77,8 +87,11 @@ const Questions = ({ component, data, handleChange, options, answer }) => {
             />{" "}
             <span className="font-bold text-[16px]">Question {component}:</span>
           </span>
-          <h5 dangerouslySetInnerHTML={{ __html: sanitizedText }} className="pt-3 mb-10"/>
-            
+          <h5
+            dangerouslySetInnerHTML={{ __html: sanitizedText }}
+            className="pt-3 mb-10"
+          />
+
           <ul className="space-y-4">
             {options.map((item, index) => (
               <li
@@ -87,7 +100,12 @@ const Questions = ({ component, data, handleChange, options, answer }) => {
                   selectedItem === item &&
                   "dark:bg-[#20232a] dark:border-[#20232a] bg-black text-[#FAEBD7]"
                 } dark:border-[white]`}
-                style={{borderColor: selectedItem === item && currentMode == "Dark" ? "#20232a" : ""}}
+                style={{
+                  borderColor:
+                    selectedItem === item && currentMode == "Dark"
+                      ? "#20232a"
+                      : "",
+                }}
                 onClick={() => handleItemClick(item)}>
                 <div className="w-4 h-4 border border-black dark:border-[#FFF8DC] rounded-full mr-2">
                   {selectedItem === item && (
@@ -98,9 +116,21 @@ const Questions = ({ component, data, handleChange, options, answer }) => {
               </li>
             ))}
           </ul>
+          <div className="flex gap-4 float-right">
+            <button
+              onClick={() => handlePrev(page)}
+              className="flex-1 px-4 py-2 rounded-sm bg-transparent hover:bg-gray-100 text-gray-600 hover:text-gray-800 border border-gray-300 hover:border-gray-400 transition duration-300">
+              Previous
+            </button>
+            <button
+              onClick={() => handleNext(page)}
+              className="flex-1 px-4 py-2 rounded-sm bg-transparent hover:bg-gray-100 text-gray-600 hover:text-gray-800 border border-gray-300 hover:border-gray-400 transition duration-300">
+              Next
+            </button>
+          </div>
         </div>
 
-        <div className=" bg-gray-50 shadow dark:text-gray-200 dark:bg-main-dark-bg rounded-sm pt-6 pb-6 mt-3">
+        <div className=" bg-gray-50 shadow dark:text-gray-200 dark:bg-main-dark-bg rounded-sm pt-6 pb-6 mt-3 relative">
           <div className="flex justify-between mb-3 mt-0 px-6">
             <span className="flex flex-row items-center">
               <span className="text-xl font-semibold">All questions</span>
@@ -111,90 +141,28 @@ const Questions = ({ component, data, handleChange, options, answer }) => {
               </button>
             </span>
           </div>
-          <hr className="mb-3 mt-2 darK:hr__border" />
-          <ul className=" list-none p-0">
-            <li
-              className="flex flex-row items-center cursor-pointer px-6 pb-1 mt-2"
-              onClick={handleClick}>
-              <Star
+          <hr className="mb-3 mt-2 dark:hr__border" />
+          <ul className=" list-none p-0 mb-10">
+            {[...Array(5)].map((_, index) => (
+              <QuestionListAnswerPanel
+                key={index}
+                questionNumber={index + 1}
+                fillColor={fillColor}
                 currentMode={currentMode}
                 currentColor={currentColor}
-                fillColor={fillColor}
-              />{" "}
-              <span className="font-bold text-[16px] mr-3">Question 1</span>{" "}
-              <span className="text-xs cursor-pointer hover:drop-shadow-xl font-semibold rounded-lg w-18 bg-orange-400 pb-1 px-2 text-white ">
-                Skipped
-              </span>
-            </li>
-            <li
-              className={`flex flex-row items-center cursor-pointer px-6 faded-li ${
-                currentMode == "Dark" ? "faded-li_dark" : "faded-li_light"
-              } pt-1`}
-              onClick={handleClick}>
-              <Star
-                currentMode={currentMode}
-                currentColor={currentColor}
-                fillColor={fillColor}
-              />{" "}
-              <span className="font-bold text-[16px]">Question 2</span>
-            </li>
-            <li
-              className={`flex flex-row items-center cursor-pointer px-6 faded-li ${
-                currentMode == "Dark" ? "faded-li_dark" : "faded-li_light"
-              } `}
-              onClick={handleClick}>
-              <Star
-                currentMode={currentMode}
-                currentColor={currentColor}
-                fillColor={fillColor}
-              />{" "}
-              <span className="font-bold text-[16px]">Question 3</span>
-            </li>
-            <li
-              className={`flex flex-row items-center cursor-pointer px-6 faded-li ${
-                currentMode == "Dark" ? "faded-li_dark" : "faded-li_light"
-              } `}
-              onClick={handleClick}>
-              <Star
-                currentMode={currentMode}
-                currentColor={currentColor}
-                fillColor={fillColor}
-              />{" "}
-              <span className="font-bold text-[16px]">Question 4</span>
-            </li>
-            <li
-              className={`flex flex-row items-center cursor-pointer px-6 faded-li ${
-                currentMode == "Dark" ? "faded-li_dark" : "faded-li_light"
-              }`}
-              onClick={handleClick}>
-              <Star
-                currentMode={currentMode}
-                currentColor={currentColor}
-                fillColor={fillColor}
-              />{" "}
-              <span className="font-bold text-[16px]">Question 5</span>
-            </li>
-            <li
-              className={`flex flex-row items-center cursor-pointer px-6 faded-li ${
-                currentMode == "Dark" ? "faded-li_dark" : "faded-li_light"
-              }`}
-              onClick={handleClick}>
-              <Star
-                currentMode={currentMode}
-                currentColor={currentColor}
-                fillColor={fillColor}
-              />{" "}
-              <span className="font-bold text-[16px]">Question 6</span>
-            </li>
+                handleClick={handleClick}
+                component={component}
+                selectedQuestion={selectedQuestion}
+              />
+            ))}
           </ul>
-
           <div className="flex justify-between items-center mt-5 border-t-1 border-color">
-            <p className=" text-gray-500 text-sm px-6">2 questions skipped</p>
+            <p className=" text-gray-500 text-sm px-6 py-3">
+              2 questions skipped
+            </p>
           </div>
         </div>
       </div>
-
-      <button onClick={handleChange}>Submit</button>
     </div>
   );
 };
