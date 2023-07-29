a38f35e9e004893e4f560e47a6763d8fd4a1eda1
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-29 19:42:42 +0100

    Update StudentProgressBar.tsx

--- src/components/Common/StudentProgressBar/StudentProgressBar.tsx
+++ src/components/Common/StudentProgressBar/StudentProgressBar.tsx
@@ -1,27 +1,54 @@
+import React, { useState, useEffect } from "react";
+import CountUp from "../CountUp/CountUp";
 import "./StudentProgressBar.css";
 
-const StudentProgressBar = () => {
+interface StudentProgressBarProps {
+  progress: number;
+  spanText: string;
+  progressText: string;
+}
+
+const StudentProgressBar: React.FC<StudentProgressBarProps> = ({
+  progress,
+  spanText,
+  progressText,
+}) => {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const newOffset = 440 - (440 * progress) / 100;
+    setOffset(newOffset);
+  }, [progress]);
+
   return (
     <>
       <div className="mt-10">
         <div className="container">
           <div className="card">
-            <div className="percent">
+            <div
+              className="percent"
+              style={{ "--num": `${progress}`, "--offset": offset }}
+            >
               <div className="dot"></div>
               <svg>
-                <circle cx={70} cy={70} r={70}></circle>
-                <circle cx={70} cy={70} r={70}></circle>
+                <circle cx={70} cy={70} r={70} stroke="#191919"></circle>
+                <circle
+                  cx={70}
+                  cy={70}
+                  r={70}
+                  style={{ opacity: 1 }}
+                ></circle>
               </svg>
               <div className="number dark:text-white text-black">
-                <h2 className="dark:text-white text-black">85<span className="dark:text-white text-black">%</span></h2>
-                <p className="dark:text-white text-black">Progress</p>
+                <h2 className="dark:text-white text-black">
+                  <CountUp start={0} end={progress} duration={2000} />
+                  <span className="dark:text-white text-black">{spanText}</span>
+                </h2>
+                <p className="dark:text-white text-black">{progressText}</p>
               </div>
             </div>
           </div>
-          
         </div>
-  
-
       </div>
     </>
   );
