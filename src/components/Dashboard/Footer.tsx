aac406f0976ad688300815726d03216619e4316a
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-29 08:11:53 +0100

    Update Footer.tsx

--- src/components/Dashboard/Footer.tsx
+++ src/components/Dashboard/Footer.tsx
@@ -1,8 +1,8 @@
-
+const currentYear = new Date().getFullYear();
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2022 All rights reserved by Shoppy.com
+      Copyright Ⓒ {currentYear} All rights reserved.
     </p>
   </div>
 );
