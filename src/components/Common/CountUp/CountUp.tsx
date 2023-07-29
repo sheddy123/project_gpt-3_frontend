4e35ec26893b7029c4128890a1ac363bd4e1e76a
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-07-29 19:42:36 +0100

    Create CountUp.tsx

---
+++ src/components/Common/CountUp/CountUp.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect, useRef } from "react";
+
+const CountUp = ({ start = 0, end, duration = 2000  }) => {
+  const [value, setValue] = useState(0);
+  const ref = useRef(start);
+
+  //const counter = end / 200;
+  const counter = end / (duration / 70); 
+
+  const Count = () => {
+    if (ref.current < end) {
+      const result = Math.ceil(ref.current + counter);
+      if (result > end) return setValue(end);
+      setValue(result);
+      ref.current = result;
+    }
+    setTimeout(Count, 70);
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      Count();
+    }
+    return () => (isMounted = false);
+  }, [end]);
+
+  return (
+    <>{value}</>
+  );
+};
+
+export default CountUp;
