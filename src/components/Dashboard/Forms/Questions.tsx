519da9622c64a9735625af79a7a09900862c5fa5
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-01 07:20:22 +0100

    Update Questions.tsx

--- src/components/Dashboard/Forms/Questions.tsx
+++ src/components/Dashboard/Forms/Questions.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import DOMPurify from "dompurify";
 import QuestionListAnswerPanel from "./QuestionListAnswerPanel";
 import { useDispatch } from "react-redux";
+import { HtmlEditor, Inject, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 
 const Questions = ({
   component,
@@ -31,6 +32,16 @@ const Questions = ({
   const [selectedItem, setSelectedItem] = useState(null);
   const sanitizedText = DOMPurify.sanitize(data.question);
   const dispatch = useDispatch();
+
+  let toolbarSettings: object = {
+    items: ['Bold', 'Italic', 'Underline', 'StrikeThrough',
+          'FontName', 'FontSize', 'FontColor', 'BackgroundColor',
+          'LowerCase', 'UpperCase', '|',
+          'Formats', 'Alignments', 'OrderedList', 'UnorderedList',
+          'Outdent', 'Indent', '|',
+          'CreateLink', 'Image', '|', 'ClearFormat', 'Print',
+          'SourceCode', 'FullScreen', '|', 'Undo', 'Redo']
+  }
 
   const currentQuestionAnsweredObject = selectAnsweredQuestions.find(
     (item) => item.formId === component + 1
@@ -80,6 +91,45 @@ const Questions = ({
         </h4>
       </div>
 
+      <RichTextEditorComponent height={450} toolbarSettings={toolbarSettings}>
+      <p>The Rich Text Editor component is WYSIWYG ("what you see is what you get") editor that provides the best user experience to create and update the content.
+        Users can format their content using standard toolbar commands.</p>
+      <p><b>Key features:</b></p>
+      <ul>
+        <li>
+          <p>Provides &lt;IFRAME&gt; and &lt;DIV&gt; modes</p>
+        </li>
+        <li>
+          <p>Capable of handling markdown editing.</p>
+        </li>
+        <li>
+          <p>Contains a modular library to load the necessary functionality on demand.</p>
+        </li>
+        <li>
+          <p>Provides a fully customizable toolbar.</p>
+        </li>
+        <li>
+          <p>Provides HTML view to edit the source directly for developers.</p>
+        </li>
+        <li>
+          <p>Supports third-party library integration.</p>
+        </li>
+        <li>
+          <p>Allows preview of modified content before saving it.</p>
+        </li>
+        <li>
+          <p>Handles images, hyperlinks, video, hyperlinks, uploads, etc.</p>
+        </li>
+        <li>
+          <p>Contains undo/redo manager.</p>
+        </li>
+        <li>
+          <p>Creates bulleted and numbered lists.</p>
+        </li>
+      </ul>
+      <Inject services={[Toolbar, HtmlEditor]} />
+    </RichTextEditorComponent>
+    
       <div className={`grid grid-cols-1 sm:grid-cols-3 gap-4`}>
         <div className="col-span-2  dark:text-gray-200  py-4 px-6 rounded-md p-3 mt-3">
           {isSubmitted && (
