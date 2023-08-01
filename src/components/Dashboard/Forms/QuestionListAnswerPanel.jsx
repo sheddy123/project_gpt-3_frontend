ad3a213b1205f3f9dff6fdbd72bc9737c366d84f
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-01 19:28:44 +0100

    Update QuestionListAnswerPanel.jsx

--- src/components/Dashboard/Forms/QuestionListAnswerPanel.jsx
+++ src/components/Dashboard/Forms/QuestionListAnswerPanel.jsx
@@ -10,15 +10,15 @@ const QuestionListAnswerPanel = React.memo(
     component,
     selectedQuestion,
     selectQuestionsSkipped,
-    isSubmitted
+    isSubmitted,
+    id,
+    selectAnsweredQuestions
   }) => { const questionAnswerPanelClass = `${
     currentMode == "Dark" ? "faded-li_dark" : "faded-li_light"
   }`;
   const isSelected = selectedQuestion.includes(questionNumber);// || questionNumber == component;
-  
   const currentComponentClass = questionNumber == component ? "bg-black text-white dark:bg-white dark:text-black" : "";
   //const currentComponentClass = questionNumber == component && selectQuestionsSkipped.length == 1 ? "bg-black text-white dark:bg-white dark:text-black" : "";
-  console.log("Component is ", component, "Question Number", questionNumber, "selectQuestionsSkipped " , selectQuestionsSkipped)
   return (
     <>
       {isSelected && !isSubmitted  ? (
@@ -28,10 +28,10 @@ const QuestionListAnswerPanel = React.memo(
           <Star
             currentMode={currentMode}
             currentColor={currentColor}
-            fillColor={selectQuestionsSkipped.includes(questionNumber) ? "fill-[#9ca8a7]" : "fill-[#14b8a6]"}
+            fillColor={selectQuestionsSkipped.includes(id) ? "fill-[#9ca8a7]" : "fill-[#14b8a6]"}
           />{" "}
           <span className="font-bold text-[16px] mr-3">Question {questionNumber}</span>{" "}
-         {selectQuestionsSkipped.includes(questionNumber) && <span className="text-xs cursor-pointer hover:drop-shadow-xl font-semibold rounded-lg w-18 bg-orange-400 pb-1 px-2 text-white ">
+         {selectQuestionsSkipped.includes(id) && <span className="text-xs cursor-pointer hover:drop-shadow-xl font-semibold rounded-lg w-18 bg-orange-400 pb-1 px-2 text-white ">
             Skipped
           </span>}
         </li>
@@ -42,11 +42,11 @@ const QuestionListAnswerPanel = React.memo(
           <Star
             currentMode={currentMode}
             currentColor={currentColor}
-            fillColor={!selectQuestionsSkipped.includes(questionNumber) ? "fill-[#14b8a6]" : `fill-[#9ca8a7]`}
+            fillColor={selectAnsweredQuestions.includes(id) ? "fill-[#14b8a6]" : `fill-[#9ca8a7]`}
           />{" "}
           <span className="font-bold text-[16px]">Question {questionNumber}</span>
           {/* {console.log("Current question is ", JSON.stringify(selectQuestionsSkipped), questionNumber, selectQuestionsSkipped.includes(questionNumber))} */}
-          {selectQuestionsSkipped.includes(questionNumber) && <span className="text-xs cursor-pointer hover:drop-shadow-xl font-semibold rounded-lg w-18 bg-orange-400 pb-1 px-2 text-white ">
+          {selectQuestionsSkipped.includes(id) && <span className="text-xs cursor-pointer hover:drop-shadow-xl font-semibold rounded-lg w-18 bg-orange-400 pb-1 px-2 text-white ">
             Skipped
           </span>}
         </li>
