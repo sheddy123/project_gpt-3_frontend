9603085491beff338843efcbdda126d1c3fc9fe0
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-04 08:47:51 +0100

    Create datasource.jsx

---
+++ src/components/Dashboard/Instructor/datasource.jsx
@@ -0,0 +1,11 @@
+export let data = [
+    {
+      Title: "Array",
+      Languages: "English",
+      Captions: true,
+      Version: 1,
+      Description: "Just a course description",
+      Course_Code: "CSC201",
+    },
+  ];
+  
