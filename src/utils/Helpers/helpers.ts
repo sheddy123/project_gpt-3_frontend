5494eba5a345949f8ed4b3847fd5066057ad241f
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-05 11:51:37 +0100

    Update helpers.ts

--- src/utils/Helpers/helpers.ts
+++ src/utils/Helpers/helpers.ts
@@ -1,9 +1,18 @@
 export function trimAndConvertToLowerCase(str) {
-    // Remove whitespace characters using trim()
-   const trimmedStr = str.replace(/\s/g, '');
+  // Remove whitespace characters using trim()
+  const trimmedStr = str.replace(/\s/g, "");
 
   // Convert the string to lowercase using toLowerCase()
   const lowercaseStr = trimmedStr.toLowerCase();
 
   return lowercaseStr;
+}
+
+export function getNonEmptyProperty(obj) {
+  for (const prop in obj) {
+    if (obj.hasOwnProperty(prop) && obj[prop] !== "") {
+      return prop;
+    }
+  }
+  return null; // Return null if no non-empty property is found
 }
