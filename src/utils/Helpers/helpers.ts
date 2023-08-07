8197c0eba5564cb9bdb789d8c620d542e28f1405
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-07 08:11:26 +0100

    Update helpers.ts

--- src/utils/Helpers/helpers.ts
+++ src/utils/Helpers/helpers.ts
@@ -16,3 +16,13 @@ export function getNonEmptyProperty(obj) {
   }
   return null; // Return null if no non-empty property is found
 }
+
+export const validateRequired = (value) => !!value.length;
+export const validateEmail = (email) =>
+  !!email.length &&
+  email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+export const validateAge = (age) => age >= 18 && age <= 50;
