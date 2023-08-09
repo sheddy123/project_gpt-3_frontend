4051e807745d349190bb340bc76c3f290a14c2b4
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-09 11:57:41 +0100

    Update DynamicForm.tsx

--- src/components/DynamicForm/DynamicForm.tsx
+++ src/components/DynamicForm/DynamicForm.tsx
@@ -15,14 +15,24 @@ import {
   IToolbarSettings,
 } from "@syncfusion/ej2-react-richtexteditor";
 
-const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoading, initialValues, currentMode }) => {
+const DynamicForm: React.FC<DynamicFormProps> = ({
+  formFields,
+  onSubmit,
+  isLoading,
+  initialValues,
+  currentMode,
+}) => {
   const { currentColor } = useStateContext();
+  const [additionalInputs, setAdditionalInputs] = useState<{
+    [key: string]: string[];
+  }>({});
+
   // const [formValues, setFormValues] = useState<{
   //   [key: string]: string | boolean;
   // }>(
   //   formFields.fields.reduce((acc, field) => ({ ...acc, [field.name]: "" }), {})
   // );
-  
+
   const [formValues, setFormValues] = useState<{
     [key: string]: string | string[];
   }>(
@@ -32,8 +42,8 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
         [field.name]: field.type === FieldType.Checkbox ? [] : "",
       }),
       {}
-    ) 
-);
+    )
+  );
 
   const [errors, setErrors] = useState<{ [key: string]: string }>(
     formFields.fields.reduce((acc, field) => ({ ...acc, [field.name]: "" }), {})
@@ -48,19 +58,16 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
     }
   };
 
-
-
   useEffect(() => {
-    
     // Initialize formValues with initial Richtext values
     const initialRichtextValues = formFields.fields
-    .filter((field) => field.type === FieldType.RichText)
-    .reduce((acc, field) => ({ ...acc, [field.name]: "" }), {});
-  setFormValues((prevValues) => ({
-    ...prevValues,
-    ...initialRichtextValues,
-    ...initialValues, // Apply initial values from the initialValues prop
-  }));
+      .filter((field) => field.type === FieldType.RichText)
+      .reduce((acc, field) => ({ ...acc, [field.name]: "" }), {});
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      ...initialRichtextValues,
+      ...initialValues, // Apply initial values from the initialValues prop
+    }));
 
     // Clean up event listeners
     return () => {
@@ -123,30 +130,45 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
     }
   };
 
+  const handleAddMore = (fieldName: string) => {
+    setAdditionalInputs((prevInputs) => ({
+      ...prevInputs,
+      [fieldName]: [...(prevInputs[fieldName] || []), ""],
+    }));
+  };
+  const getAdditionalFields = () => {
+    const additionalFields = [];
+    console.log("getAdditionalFields", additionalInputs);
+    for (const field of formFields.fields) {
+      if (field.addMore && Object.keys(additionalInputs).length > 0) {
+        additionalFields.push(...additionalInputs[field.name]);
+      }
+    }
+    return additionalFields;
+  };
+
+  const getAdditionalFieldValues = () => {
+    const additionalValues: { [key: string]: string } = {};
+    for (const field of formFields.fields) {
+      if (field.addMore && Object.keys(additionalInputs).length > 0) {
+        additionalValues[field.name] = additionalInputs[field.name].join(",");
+      }
+      console.log("Fields is ", field);
+    }
+    return additionalValues;
+  };
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const requiredFields = formFields.fields.filter((field) => field.required);
+
+    const allFields = [...formFields.fields, ...getAdditionalFields()];
 
     let fieldErrors = {};
-    for (const field of requiredFields) {
+    for (const field of allFields) {
       const value = formValues[field.name];
 
-      // Check for missing values based on the field type
-      if (
-        (field.type === FieldType.Input || field.type === FieldType.RichText) &&
-        !value
-      ) {
+      if (field.required && !value) {
         fieldErrors[field.name] = "Field is required";
-      } else if (
-        field.type === FieldType.Select &&
-        (value === "" || value === undefined)
-      ) {
-        fieldErrors[field.name] = "Please select an option";
-      } else if (
-        field.type === FieldType.Radio &&
-        (value === "" || value === undefined)
-      ) {
-        fieldErrors[field.name] = "Please select an option";
       } else if (
         field.type === FieldType.Checkbox &&
         (!Array.isArray(value) || value.length === 0)
@@ -154,14 +176,39 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
         fieldErrors[field.name] = "Please select at least one option";
       }
     }
-    // Set the field errors
+
     setErrors(fieldErrors);
-    // If there are any errors, stop the submission
+
     if (Object.keys(fieldErrors).length > 0) {
       return;
     }
+
+    const allFormValues = {
+      ...formValues,
+      ...getAdditionalFieldValues(),
+    };
+
     // Proceed with the submission if there are no errors
-    onSubmit(formValues);
+    onSubmit(allFormValues);
+  };
+
+  const handleAdditionalInputChange =
+    (fieldName: string, index: number) =>
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      setAdditionalInputs((prevInputs) => ({
+        ...prevInputs,
+        [fieldName]: prevInputs[fieldName].map((value, i) =>
+          i === index ? newValue : value
+        ),
+      }));
+    };
+
+  const handleDeleteAdditionalInput = (fieldName: string, index: number) => {
+    setAdditionalInputs((prevInputs) => ({
+      ...prevInputs,
+      [fieldName]: prevInputs[fieldName].filter((_, i) => i !== index),
+    }));
   };
 
   return (
@@ -177,35 +224,92 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
           {formFields.fields.map((field) => (
             <>
               {field.type === FieldType.Input && (
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor={field.name}
-                    className={`${currentMode == "Dark" ? "text-slate-100" : "text-gray-900" } block  text-sm  leading-6 font-semibold`}
-                    >
-                    {field.label}{" "}
-                    {field.required && <span className="text-red-500">*</span>}
-                  </label>
-                  <input
-                    type="text"
-                    id={field.name}
-                    name={field.name}
-                    className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
-                    value={formValues[field.name] as string}
-                    onChange={handleChange}
-                    //  required={field.required}
-                  />
-                  {errors[field.name] && (
-                    <span className="text-red-500 text-sm font-semibold">
-                      {errors[field.name]}
-                    </span>
+                <>
+                  <div className="sm:col-span-3">
+                    <label
+                      htmlFor={field.name}
+                      className={`${
+                        currentMode == "Dark"
+                          ? "text-slate-100"
+                          : "text-gray-900"
+                      } block  text-sm  leading-6 font-semibold`}>
+                      {field.label}{" "}
+                      {field.required && (
+                        <span className="text-red-500">* </span>
+                      )}
+                      {errors[field.name] && (
+                        <span className="text-red-500 text-sm font-semibold">
+                          {errors[field.name]}
+                        </span>
+                      )}
+                    </label>
+                    <input
+                      type="text"
+                      id={field.name}
+                      name={field.name}
+                      className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+                      value={formValues[field.name] as string}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  {/* This is used to add same input type if addMore is set to true */}
+
+                  {field.addMore && (
+                    <button
+                      type="button"
+                      onClick={() => handleAddMore(field.name)}
+                      className="rounded-md mt-4 dark:text-white px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                      style={{ background: currentColor }}>
+                      Add more
+                    </button>
                   )}
-                </div>
+                  {additionalInputs[field.name] &&
+                    additionalInputs[field.name].map((value, index) => (
+                      <div key={index} className="sm:col-span-3">
+                        <label
+                          htmlFor={field.name}
+                          className={`${
+                            currentMode == "Dark"
+                              ? "text-slate-100"
+                              : "text-gray-900"
+                          } block  text-sm  leading-6 font-semibold`}>
+                          Additional {field.name}&nbsp;
+                          {index + 1} &nbsp;
+                          {field.required && (
+                            <span className="text-red-500">*</span>
+                          )}
+                        </label>
+                        <input
+                          type="text"
+                          id={`${field.name}_additional_${index}`}
+                          name={`${field.name}_additional_${index}`}
+                          className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                          value={value}
+                          required={field.required}
+                          onChange={handleAdditionalInputChange(
+                            field.name,
+                            index
+                          )}
+                        />
+                        <button
+                          type="button"
+                          onClick={() =>
+                            handleDeleteAdditionalInput(field.name, index)
+                          }
+                          className="rounded-md mt-2 px-3 py-1.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-red-500">
+                          Delete
+                        </button>
+                      </div>
+                    ))}
+                </>
               )}
               {field.type === FieldType.TextArea && (
                 <div className="col-span-full">
                   <label
                     htmlFor={field.name}
-                    className={`${currentMode == "Dark" ? "text-slate-100" : "text-gray-900" } block  text-sm  leading-6 font-semibold`}>
+                    className={`${
+                      currentMode == "Dark" ? "text-slate-100" : "text-gray-900"
+                    } block  text-sm  leading-6 font-semibold`}>
                     {field.name}{" "}
                     {field.required && <span className="text-red-500">*</span>}
                   </label>
@@ -229,17 +333,25 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
                 <div className="col-span-full">
                   <label
                     htmlFor={field.name}
-                    className={`${currentMode == "Dark" ? "text-slate-100" : "text-gray-900" } block  text-sm  leading-6 font-semibold`}>
+                    className={`${
+                      currentMode == "Dark" ? "text-slate-100" : "text-gray-900"
+                    } block  text-sm  leading-6 font-semibold`}>
                     {field.name}{" "}
-                    {field.required && <span className="text-red-500">*</span>}
+                    {field.required && <span className="text-red-500">* </span>}
+                    {errors[field.name] && (
+                      <span className="text-red-500 text-sm font-semibold">
+                        {errors[field.name]}
+                      </span>
+                    )}
                   </label>
+
                   <RichTextEditorComponent
                     id={field.name}
                     name={field.name}
                     className="form-control"
                     height={350}
                     showCharCount={true}
-                    maxLength={100}
+                    //maxLength={100}
                     placeholder={"Type something"}
                     toolbarSettings={field?.toolbarSettings as IToolbarSettings}
                     value={formValues[field.name] as string}
@@ -256,20 +368,17 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
                       ]}
                     />
                   </RichTextEditorComponent>
-                  {errors[field.name] && (
-                    <span className="text-red-500 text-sm font-semibold">
-                      {errors[field.name]}
-                    </span>
-                  )}
                 </div>
               )}
               {field.type === FieldType.Select && (
                 <div className="sm:col-span-3">
                   <label
                     htmlFor={field.name}
-                    className={`${currentMode == "Dark" ? "text-slate-100" : "text-gray-900" } block  text-sm  leading-6 font-semibold`}>
+                    className={`${
+                      currentMode == "Dark" ? "text-slate-100" : "text-gray-900"
+                    } block  text-sm  leading-6 font-semibold`}>
                     {field.label}{" "}
-                    {field.required && <span className="text-red-500">*</span>}
+                    {field.required && <span className="text-red-500">* </span>}
                     {errors[field.name] && (
                       <span className="text-red-500 text-sm font-semibold">
                         {errors[field.name]}
@@ -284,7 +393,10 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
                     className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6">
                     <option value="">Select an option</option>
                     {field.options?.map((option) => (
-                      <option key={option.id} value={option.value} selected={formValues[field.name] === option.value}>
+                      <option
+                        key={option.id}
+                        value={option.value}
+                        selected={formValues[field.name] === option.value}>
                         {option.value}
                       </option>
                     ))}
@@ -293,9 +405,12 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
               )}
               {field.type === FieldType.Radio && (
                 <div className="space-y-2 sm:col-span-3">
-                  <legend className={`${currentMode == "Dark" ? "text-slate-100" : "text-gray-900" } block  text-sm  leading-6 font-semibold`}>
+                  <legend
+                    className={`${
+                      currentMode == "Dark" ? "text-slate-100" : "text-gray-900"
+                    } block  text-sm  leading-6 font-semibold`}>
                     {field.label}{" "}
-                    {field.required && <span className="text-red-500">*</span>}
+                    {field.required && <span className="text-red-500">* </span>}
                     {errors[field.name] && (
                       <span className="text-red-500 text-sm">
                         {errors[field.name]}
@@ -317,7 +432,11 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
                       <label
                         key={option.value}
                         htmlFor={`${field.name}_${option.id}`} // Add htmlFor attribute to associate label with input
-                        className={`block text-sm font-medium leading-6 ${currentMode == "Dark" ? "text-slate-100" : "text-gray-900" }`}>
+                        className={`block text-sm font-medium leading-6 ${
+                          currentMode == "Dark"
+                            ? "text-slate-100"
+                            : "text-gray-900"
+                        }`}>
                         {option.value}
                       </label>
                     </div>
@@ -326,10 +445,14 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
               )}
 
               {field.type === FieldType.Checkbox && (
-                <div className={`${currentMode == "Dark" ? "text-slate-100" : "text-gray-900" } space-y-2 sm:col-span-3`}>
-                  <legend className={` block  text-sm  leading-6 font-semibold`}>
+                <div
+                  className={`${
+                    currentMode == "Dark" ? "text-slate-100" : "text-gray-900"
+                  } space-y-2 sm:col-span-3`}>
+                  <legend
+                    className={` block  text-sm  leading-6 font-semibold`}>
                     {field.label}{" "}
-                    {field.required && <span className="text-red-500">*</span>}
+                    {field.required && <span className="text-red-500">* </span>}
                     {errors[field.name] && (
                       <span className="text-red-500 text-sm">
                         {errors[field.name]}
@@ -354,7 +477,11 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ formFields, onSubmit, isLoadi
                           />
                           <label
                             htmlFor={`${field.name}_${option.id}`} // Add htmlFor attribute to associate label with input
-                            className={`ml-2 ${currentMode == "Dark" ? "text-slate-100" : "text-gray-900" }`}>
+                            className={`ml-2 ${
+                              currentMode == "Dark"
+                                ? "text-slate-100"
+                                : "text-gray-900"
+                            }`}>
                             {option.value}
                           </label>
                         </div>
