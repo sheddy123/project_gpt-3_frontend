cadb28d0b28c3048d9e6b5e12f2d09ec1a33f56d
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-09 11:57:39 +0100

    Update api_constants.tsx

--- src/utils/Constants/ApiConstants/api_constants.tsx
+++ src/utils/Constants/ApiConstants/api_constants.tsx
@@ -2,7 +2,8 @@ export const BaseUrl = import.meta.env.VITE_API_BASE_URL;
 export const AUTH_TOKEN = "/";
 export const Login_Url = BaseUrl + import.meta.env.VITE_LOGIN;
 export const Logout_Url = BaseUrl + import.meta.env.VITE_LOGOUT;
-export const Create_Profile_Url = BaseUrl + import.meta.env.VITE_CREATE_PROFILE_URL;
+export const Create_Profile_Url =
+  BaseUrl + import.meta.env.VITE_CREATE_PROFILE_URL;
 export const Get_Profile_Url = BaseUrl + import.meta.env.VITE_PROFILE_URL;
 export const Course_Url = BaseUrl + import.meta.env.VITE_COURSE_URL;
 export const Questions_Url = BaseUrl + import.meta.env.VITE_QUESTIONS_URL;
@@ -13,14 +14,13 @@ export const ROLES = {
   Student: "Student",
 };
 
-
 export enum FieldType {
   Input = "input",
   TextArea = "textArea",
   Select = "select",
   Radio = "radio",
   Checkbox = "checkbox",
-  RichText = "richText"
+  RichText = "richText",
 }
 export enum FieldLabel {
   Course = "Course",
@@ -31,7 +31,7 @@ export enum FieldLabel {
   question_Type = "Question_Type",
   Question = "Question",
   Options = "Options",
-  QuestionId = "QuestionId"
+  QuestionId = "QuestionId",
 }
 export enum FieldName {
   Course = "Course",
@@ -42,4 +42,11 @@ export enum FieldName {
   QuestionType = "Question Type",
   Question = "Question",
   MultipleOptions = "Multiple Options",
+  Options = "Options",
+}
+
+export enum CourseStatus {
+  Active = "Active",
+  Pending = "Pending",
+  Completed = "Completed",
 }
