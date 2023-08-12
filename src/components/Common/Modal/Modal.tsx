5adfe47d779f0196bdb5161e02f5e223fc5f0d37
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-12 19:36:01 +0100

    Update Modal.tsx

--- src/components/Common/Modal/Modal.tsx
+++ src/components/Common/Modal/Modal.tsx
@@ -1,24 +1,24 @@
-import { useDispatch, useSelector } from "react-redux";
-import "./Modal.css";
-import { closeModal } from "@/redux/features/modal/modalSlice";
-import { Close } from "../Icons/Icons";
+import { useSelector } from "react-redux";
+
 import OAuthGithub from "../OAuthProviders/OAuthGithub";
 import OAuthGoogle from "../OAuthProviders/OAuthGoogle";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import CustomModal from "./CustomModal";
 
 const Modal = () => {
-  const dispatch = useDispatch();
   const [errorText, setErrorText] = useState("");
   const errorResponse = useSelector((store) => store?.authReducer?.message);
   const [showAlert, setShowAlert] = useState(false);
   const location = useLocation();
   const message = location.state && location.state.message;
-  
+
   useEffect(() => {
     if (
       typeof errorResponse === "string" &&
-      (errorResponse.includes("Lifetime validation failed") || message === "Not Authenticated" || message === "Session Ended")
+      (errorResponse.includes("Lifetime validation failed") ||
+        message === "Not Authenticated" ||
+        message === "Session Ended")
     ) {
       // The string "Lifetime validation failed" is present in the variable
       setErrorText("Session Ended");
@@ -34,40 +34,29 @@ const Modal = () => {
   //console.log("The variable contains 'Lifetime validation failed'" + errorText);
 
   return (
-    <aside className="modal-container ">
-      <div className="modal ">
-        {showAlert && (
-          <div
-            className={`bg-red-100 border alert-container animate__animated  ${
-              showAlert ? "animate__fadeInDown" : "animate__fadeOutUp"
-            } border-red-400 text-red-700 px-4 py-3 rounded relative`}
-            role="alert">
-            <strong className="font-bold">{message}</strong>
-          </div>
-        )}
-        <div className="header">
-          <h4>Signup with one of the following:</h4>
-
-          <button
-            className="close-icon hover:drop-shadow-xl hover:bg-light-gray"
-            style={{ borderRadius: "50%" }}
-            onClick={() => dispatch(closeModal(null))}>
-            <Close />
-          </button>
+    <CustomModal showCloseIcon={true} maxWidth={''} styles={''}>
+      {showAlert && (
+        <div
+          className={`bg-red-100 border alert-container animate__animated  ${
+            showAlert ? "animate__fadeInDown" : "animate__fadeOutUp"
+          } border-red-400 text-red-700 px-4 py-3 rounded relative`}
+          role="alert">
+          <strong className="font-bold">{message}</strong>
         </div>
-        <div className="content">
-          {/* <div className="mt-4">
+      )}
+
+      <div className="content">
+        {/* <div className="mt-4">
             <OAuthMicrosoft />
           </div> */}
-          <div className="mt-2">
-            <OAuthGithub />
-          </div>
-          <div className="mt-2">
-            <OAuthGoogle />
-          </div>
+        <div className="mt-2">
+          <OAuthGithub />
+        </div>
+        <div className="mt-2">
+          <OAuthGoogle />
         </div>
       </div>
-    </aside>
+    </CustomModal>
   );
 };
 export default Modal;
