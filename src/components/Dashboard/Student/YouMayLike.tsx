b7852d906063cf54fff0b55d78d6d1ac1088fc3a
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-15 12:28:30 +0100

    Update YouMayLike.tsx

--- src/components/Dashboard/Student/YouMayLike.tsx
+++ src/components/Dashboard/Student/YouMayLike.tsx
@@ -1,12 +1,17 @@
 import { dashboardSidebar } from "@/utils/Constants/ComponentsConstants/constants";
 import { useStateContext } from "@/utils/Helpers/ContextProvider";
 import LearningHoursCard from "./LearningHoursCard";
+import { useSelector } from "react-redux";
+import { convertMillisecondsToTime } from "@/utils/Helpers/helpers";
+import { selectStudentLogTime } from "@/redux/features/auth/authSlice";
 const YouMayLike = () => {
   const { currentColor } =
     useStateContext();
   const { img } = dashboardSidebar;
+  const studentLogTime = useSelector(selectStudentLogTime);
+  const timeSpent = convertMillisecondsToTime(studentLogTime?.totalDurationMillis);
   return (
-    <LearningHoursCard currentColor={currentColor} img={img} noOfHours={76} />
+    <LearningHoursCard currentColor={currentColor} img={img} noOfHours={timeSpent} />
   );
 };
 
