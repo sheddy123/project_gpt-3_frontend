2bd0a4cc01d0d9bd27702f59cd5fff4c6475776a
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-16 08:12:03 +0100

    Update Leaders.tsx

--- src/components/Dashboard/Student/Leaders.tsx
+++ src/components/Dashboard/Student/Leaders.tsx
@@ -1,70 +1,107 @@
-import { IoIosMore } from "react-icons/io"
-import { medicalproBranding } from "../data/dummy"
-import { Button } from '@/components';
+import { IoIosMore, IoMdTrophy } from "react-icons/io";
 import { useStateContext } from "@/utils/Helpers/ContextProvider";
+import { useDispatch, useSelector } from "react-redux";
+import { selectHighestScorers } from "@/redux/features/auth/authSlice";
+import { useEffect } from "react";
+import { getHighestPerformingScorersService } from "@/services/api/AuthService/GetAuthService";
+import { getInitials } from "@/utils/Helpers/helpers";
 const Leaders = () => {
-    const { currentColor } = useStateContext();
+  const { currentColor } = useStateContext();
+  const highestScorers = useSelector(selectHighestScorers);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getHighestPerformingScorersService() as any);
+  }, []);
+
+  const difficultyClassMapping = {
+    Difficult: "bg-red-100 text-red-800",
+    Medium: "bg-green-100 text-green-800",
+    Easy: "bg-yellow-100 text-yellow-800",
+  };
+
+  function getDifficultyBadge(difficulty) {
+    const classNames = difficultyClassMapping[difficulty];
+    if (classNames) {
+      return (
+        <span
+          className={`${classNames} font-medium mr-2 px-2.5 py-0.5 text-[10px] rounded dark:${classNames.replace(
+            "bg-",
+            "dark:bg-"
+          )} dark:text-${classNames.replace("text-", "text-")}`}>
+          {difficulty}
+        </span>
+      );
+    }
+    return null;
+  }
+
+  const tableRowData =
+    highestScorers?.length > 0 ? (
+      highestScorers.map(
+        ({ user_name, course_name, difficulty_level_name, grade }) => {
+          return (
+            <tr className="border-b border-gray-200 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800 text-[10px]">
+                {course_name}
+              </th>
+              <td className="px-6 py-4 text-[10px]">{getInitials(user_name)}</td>
+              <td className="px-6 py-4 text-[10px]">{grade}</td>
+              <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800 text-[10px]">
+                {getDifficultyBadge(difficulty_level_name)}
+              </td>
+            </tr>
+          );
+        }
+      )
+    ) : (
+      <span className="mt-5">No data to display</span>
+    );
   return (
     <>
-    
-    <div className=" bg-gray-50 shadow dark:text-gray-200 dark:bg-main-dark-bg rounded-sm p-6 m-3">
-          <div className="flex justify-between">
-            <p className="text-xl font-semibold">Leaders</p>
-            <button type="button" className="text-xl font-semibold text-gray-400">
-              <IoIosMore />
-            </button>
-          </div>
-          <p className="text-xs cursor-pointer hover:drop-shadow-xl font-semibold rounded-lg w-24 bg-orange-400 py-0.5 px-2 text-gray-200 mt-10">
-            16 APR, 2021
+      <div className=" bg-gray-50 shadow dark:text-gray-200 dark:bg-main-dark-bg rounded-sm p-6 m-3">
+        <div className="flex justify-between">
+          <p className="text-xl font-semibold text-[12px]">
+            {" "}
+            <IoMdTrophy /> Highest Scorers in Different Quizzes
           </p>
-
-          <div className="flex gap-4 border-b-1 border-color mt-6">
-            {medicalproBranding.data.map((item) => (
-              <div key={item.title} className="border-r-1 border-color pr-4 pb-2">
-                <p className="text-xs text-gray-400">{item.title}</p>
-                <p className="text-sm">{item.desc}</p>
-              </div>
-            ))}
-          </div>
-          <div className="border-b-1 border-color pb-4 mt-2">
-            <p className="text-md font-semibold mb-2">Teams</p>
-
-            <div className="flex gap-4">
-              {medicalproBranding.teams.map((item) => (
-                <p
-                  key={item.name}
-                  style={{ background: item.color }}
-                  className="cursor-pointer hover:drop-shadow-xl text-white py-0.5 px-3 rounded-lg text-xs"
-                >
-                  {item.name}
-                </p>
-              ))}
-            </div>
-          </div>
-          <div className="mt-2">
-            <p className="text-md font-semibold mb-2">Leaders</p>
-            <div className="flex gap-4">
-              {medicalproBranding.leaders.map((item, index) => (
-                <img key={index} className="rounded-full w-8 h-8" src={item.image} alt="" />
-              ))}
-            </div>
-          </div>
-          <div className="flex justify-between items-center mt-5 border-t-1 border-color">
-            <div className="mt-3">
-              <Button
-                color="white"
-                bgColor={currentColor}
-                text="Add"
-                borderRadius="10px"
-              />
-            </div>
-
-            <p className="text-gray-400 text-sm">36 Recent Transactions</p>
-          </div>
+          <button type="button" className="text-xl font-semibold text-gray-400">
+            <IoIosMore />
+          </button>
         </div>
 
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 bg-gray-50 dark:bg-gray-800 text-[10px]">
+                  Quiz Name
+                </th>
+                <th scope="col" className="px-6 py-3 text-[10px]">
+                  Highest Scorer
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 bg-gray-50 dark:bg-gray-800 text-[10px]">
+                  Score
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 bg-gray-50 dark:bg-gray-800 text-[10px]">
+                  Difficulty
+                </th>
+              </tr>
+            </thead>
+            <tbody>{tableRowData}</tbody>
+          </table>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Leaders
+export default Leaders;
