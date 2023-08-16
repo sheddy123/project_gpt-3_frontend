ed8f215f55c13bda8829c780a61af89f72eed314
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2023-08-16 08:11:51 +0100

    Update MyLearning.tsx

--- src/components/Dashboard/Student/MyLearning.tsx
+++ src/components/Dashboard/Student/MyLearning.tsx
@@ -2,12 +2,18 @@ import { useStateContext } from "@/utils/Helpers/ContextProvider";
 import { ecomPieChartData, pieChartData } from "../data/dummy";
 import DropDownComponent from "./DropdownComponent";
 import { Pie, ChartsHeader } from "@/components";
-import { selectStudentLogTime, selectStudentProgress } from "@/redux/features/auth/authSlice";
+import {
+  selectStudentLogTime,
+  selectStudentProgress,
+} from "@/redux/features/auth/authSlice";
 import { useSelector } from "react-redux";
 import {
   convertMillisecondsToTime,
   convertMillisecondsToTimeToString,
+  getImageURLForBrand,
+  truncateString,
 } from "@/utils/Helpers/helpers";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 const MyLearning = () => {
   const filterData: string[] = ["By hours", "By minutes", "By seconds"];
 
@@ -15,47 +21,54 @@ const MyLearning = () => {
   const studentLogTime = useSelector(selectStudentLogTime);
   const studentProgress = useSelector(selectStudentProgress);
 
-  const transformedArray = studentProgress?.map(item => ({
+  const transformedArray = studentProgress?.map((item) => ({
     x: item.name,
     y: item.percentageProgress,
-    text: item.percentageProgress > 0 ? `${item.percentageProgress.toFixed(2)}%` : "0%"
-}));
+    text:
+      item.percentageProgress > 0
+        ? `${item.percentageProgress.toFixed(2)}%`
+        : "0%",
+  }));
 
   // Sort the array based on durationMillis in descending order
   const sortedData = studentLogTime?.getStudentLogTimeVws
-  ?.slice()
-  .sort((a, b) => b.durationMillis - a.durationMillis);
+    ?.slice()
+    .sort((a, b) => b.durationMillis - a.durationMillis);
 
-// Get distinct values based on the "durationMillis" property
-const distinctData = sortedData?.filter((item, index, self) => 
-  index === self.findIndex((t) => (
-      t.durationMillis === item.durationMillis
-  ))
-);
+  // Get distinct values based on the "durationMillis" property
+  const distinctData = sortedData?.filter(
+    (item, index, self) =>
+      index === self.findIndex((t) => t.durationMillis === item.durationMillis)
+  );
 
-// Select the first two objects with the highest duration from the distinct data
-const topTwoObjects = distinctData?.slice(0, 2);
-  const renderedDivs = topTwoObjects?.map((obj, index) => (
-    <div key={index}>
-      <figcaption className="flex items-center justify-start space-x-3">
-        <img
-          className="rounded-full w-9 h-9"
-          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
-          alt="profile picture"
-        />
-        <div className="space-y-0.5 font-medium dark:text-white text-left">
-          <div className="text-sm text-gray-500 dark:text-gray-400">
-            {obj.title}
+  // Select the first two objects with the highest duration from the distinct data
+  const topTwoObjects = distinctData?.slice(0, 2);
+  const renderedDivs = topTwoObjects?.map((obj, index) => {
+    return (
+      <div key={index}>
+        <figcaption className="flex items-center justify-start space-x-3">
+          <img
+            className="rounded-full w-9 h-9"
+            src={getImageURLForBrand(obj?.title)}
+            alt="profile picture"
+          />
+          <div className="space-y-0.5 font-medium dark:text-white text-left">
+            <TooltipComponent content={obj.title} position="BottomCenter">
+              <div className="text-sm text-gray-500 dark:text-gray-400">
+                {truncateString(obj.title, 26)}
+              </div>
+            </TooltipComponent>
+
+            <div>
+              <span className="font-bold font-poppins text-lg">
+                {convertMillisecondsToTimeToString(obj.durationMillis)}
+              </span>
+            </div>
           </div>
-          <div>
-            <span className="font-bold font-poppins text-lg">
-              {convertMillisecondsToTimeToString(obj.durationMillis)}
-            </span>
-          </div>
-        </div>
-      </figcaption>
-    </div>
-  ));
+        </figcaption>
+      </div>
+    );
+  });
   return (
     <>
       <div className="flex justify-between items-center gap-2 mt-10">
